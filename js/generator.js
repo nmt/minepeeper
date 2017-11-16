2d83eb374995175a6bf140f8e6fb37cad66120a9
Nathalia Tan <natan@deloitte.com.au>
2017-11-16 13:27:21 +1100

    Added printable rows/grid

--- js/generator.js
+++ js/generator.js
@@ -3,17 +3,24 @@ $(document).ready(generateGrid);
 var width = 8;
 var height = 10;
 
+var grid = [];
+
 /**
  * Generates the N by M grid specified by width and height parameters.
  */
 function generateGrid() {
     var row = [];
-    var grid = [];
+    var printableRow = '';
+    var printableGrid = '';
 
     for (var i = 0; i < width; i++){
-        row.push('X');
+        row.push('☐');
+        printableRow += '☐ ';
     }
     for (var i = 0; i < height; i++){
         grid.push(row);
+        // printableGrid += printableRow + '<br>';
     }
+
+    $('.grid').text(printableGrid);
 }
