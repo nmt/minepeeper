3ffa0ca4e2ab0ac9d5b57ee1c031fec600608cae
nmt <10490737+nmt@users.noreply.github.com>
2024-09-18 23:55:13 +1000

    wip: Bomb?

--- src/Components/Game/utils/generateGrid.ts
+++ src/Components/Game/utils/generateGrid.ts
@@ -1,3 +1,5 @@
+let grid: number[][] = [];
+
 export const generateGrid = ({
   width,
   height,
@@ -5,14 +7,42 @@ export const generateGrid = ({
   width: number;
   height: number;
 }): any => {
-  const array = [];
   // Height
   for (let i = 0; i < height; i++) {
     let row = [];
     for (let j = 0; j < width; j++) {
       row.push(0);
     }
-    array.push(row);
+    grid.push(row);
   }
-  return array;
+  return grid;
+};
+
+export const isBomb = (x: number, y: number): boolean => {
+  return grid[x][y] === -1;
+};
+
+const placeBombs = ({
+  bombCount,
+  width,
+  height,
+}: {
+  bombCount: number;
+  width: number;
+  height: number;
+}): void => {
+  var x, y;
+
+  for (var i = 0; i < bombCount; i++) {
+    x = Math.floor(Math.random() * height);
+    y = Math.floor(Math.random() * width);
+
+    // Only place a bomb if the area is clear!
+    if (!isBomb(x, y)) {
+      // bombList.add("#" + x + " " + y);
+      grid[x][y] = -1;
+    } else {
+      i--;
+    }
+  }
 };
