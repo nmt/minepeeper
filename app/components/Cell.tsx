786ba53bafb76840f6945874f3a41afa90f1824f
nmt <10490737+nmt@users.noreply.github.com>
2026-04-22 10:51:30 +1000

    feat: Implement Cell component

---
+++ app/components/Cell.tsx
@@ -0,0 +1,26 @@
+interface CellProps {
+  cellType: "bomb" | "number" | "empty" | "flag";
+  cellValue?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+  isShowing: boolean;
+}
+
+export default function Cell({ cellType, cellValue, isShowing }: CellProps) {
+  const renderNumberCell = () => {
+    if (cellValue) return <div className="cell">{cellValue}</div>;
+  };
+
+  if (isShowing) {
+    switch (cellType) {
+      case "bomb":
+        return <div className="cell">💣</div>;
+      case "number":
+        return renderNumberCell();
+      case "empty":
+        return <div className="cell"> </div>;
+      case "flag":
+        return <div className="cell">🚩</div>;
+    }
+  } else {
+    return <div className="hidden">{cellValue}</div>;
+  }
+}
